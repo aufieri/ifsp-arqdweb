4d9d54c1bec2542481e6cd68cf3bbb6e9ada151b
Lucca Aufieri <aufieriz@gmail.com>
2025-05-10 18:22:56 -0300

    Classe usuario atualizado com sucesso por Lucca

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,9 +1,24 @@
 package model;
 
 public class Usuario {
-    private String userName;
+    private String userName;   
+    private String senha;
     
-    private String password;
+ // getters e setters
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
     
-    // getters e setters
+    
+    
+    
 }

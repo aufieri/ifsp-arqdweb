81f5696edf85fd3bf79083ee49714926ea595de9
Douglas <douglaswallacedev@gmail.com>
2025-07-12 14:57:34 -0300

    Douglas - Ajuste de login

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -15,36 +15,31 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 1. Recebe os dados do formulário
+		
 		String email = request.getParameter("email");
 		String senha = request.getParameter("senha");
 
 		System.out.println("[LOGIN SERVLET] Tentativa de login: " + email);
 
 		DaoUsuario dao = DaoUsuario.getInstance();
-
 		Usuario usuario = dao.buscarPorEmail(email);
 
 		if (usuario != null && usuario.getSenha().equals(senha)) {
-			// Login OK
 			System.out.println("[LOGIN SERVLET] Login OK para: " + email);
 
-			// Cria sessão e guarda usuário
 			HttpSession session = request.getSession();
 			session.setAttribute("usuarioLogado", usuario);
 
-			// Verifica se é admin
-			if (usuario.getEmail().equalsIgnoreCase("admin@examplo.com")) {
-				response.sendRedirect("CRUD.html");
-			} else {
-				response.sendRedirect("index.html");
+			switch (usuario.getTipo()) {
+				case "admin":
+					response.sendRedirect("CRUD.html");
+					break;
+				case "avaliador":
+					response.sendRedirect("index.html");
+					break;
 			}
-
 		} else {
-			// Login falhou
 			System.out.println("[LOGIN SERVLET] Falha no login para: " + email);
-
-			// Redireciona de volta para a página de login com parâmetro de erro
 			response.sendRedirect("login.html?erro=1");
 		}
 	}

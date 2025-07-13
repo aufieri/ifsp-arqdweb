eadd96eab5952522a91a7d4c55fad7a287857bd4
Lucca Aufieri <aufieriz@gmail.com>
2025-07-13 12:59:11 -0300

    Realizadas alteracoes para listar jogos no json com exito

---
+++ src/main/java/controller/ListarJogosJson.java
@@ -0,0 +1,29 @@
+package controller;
+
+import com.google.gson.Gson;
+import dao.DaoJogo;
+import model.Jogo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/api/jogos")
+public class ListarJogosJson extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private DaoJogo dao = DaoJogo.getInstance();
+	private Gson gson = new Gson();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Jogo> jogos = dao.getListaDeJogos();
+		String json = gson.toJson(jogos);
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+	}
+}
